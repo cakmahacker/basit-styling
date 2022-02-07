b1459adce58821b7f5cd31c0511b67da85ec24ae
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2022-02-08 02:40:37 +0400

    Update tema.py

--- tema.py
+++ tema.py
@@ -1,7 +1,6 @@
 import os
 os.system('cd') 
-os.system('cd ..')
-os.system('cd usr/etc')
+os.system('cd ../usr/etc')
 os.system('rm -rf bash.bashrc')
 isim = input('İsim yaz >>> ')
 alt = input('Alt yazı >>> ')
